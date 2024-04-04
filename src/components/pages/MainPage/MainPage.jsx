5ac0b7a042d5e2e9eef5d701d5135d1963f54cfc
NISSAN-180SX-NISMO <semenaksin13@gmail.com>
2024-04-05 01:15:45 +0300

    prosto pizda

--- src/components/pages/MainPage/MainPage.jsx
+++ src/components/pages/MainPage/MainPage.jsx
@@ -1,25 +1,33 @@
 import * as React from 'react';
 import {TitleCard} from "../../atoms/TitleCard/TitleCard";
 import {TitleMiniature} from "../../../API/model/TitleMiniature.tsx";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {TitleService} from "../../../API/TItleService";
 
-
 const MainPage = () => {
-    const [title: TitleMiniature, setTitle] = React.useState({});
+    const [title, setTitle] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect( () => {
         async function fetchTitle() {
             try {
+                console.log("fetching title...");
                 const response = await TitleService.getTitle(1, "name", "posterURL");
-                const data = response.json();
+                const data = await response.json();
                 console.log("data in mainpage " + data);
                 setTitle(new TitleMiniature({...data}));
+                setIsLoading(false);
             } catch (e) {
                 console.error(e);
             }
         }
         fetchTitle();
     }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
             <TitleCard
